733f5c12012bfdce90f166c3e86de3aca7a0e271
Brayden Jo <brayden.jo@outlook.com>
2020-03-15 07:28:10 +0900

    res 파일 파서

--- pyxing/res.py
+++ pyxing/res.py
@@ -1,2 +1,52 @@
-# res file parser
-RES_PATH = "c:/eBEST/xingAPI/Res"
+def parse_res(lines):
+    data = {}
+    inblock_field = []
+    outblock_field = []
+
+    parse_mode = 0        # 0: none, 1: inblock, 2: outblock
+
+    for line in lines:
+        strip_line = line.strip()
+
+        if strip_line.startswith(".Func"):
+            tokens = strip_line.split(',')
+            data['name'] = tokens[2]
+            continue
+
+        if "InBlock" in strip_line:
+            parse_mode = 1
+            continue
+
+        if "OutBlock" in strip_line:
+            parse_mode = 2
+            continue
+
+        if "END_DATA_MAP" in strip_line:
+            parse_mode = 0
+            continue
+
+        if parse_mode == 1 and strip_line != 'begin' and strip_line != 'end':
+            fields = strip_line.split(',')
+            field = fields[1]
+            inblock_field.append(field)
+
+        if parse_mode == 2 and strip_line != 'begin' and strip_line != 'end':
+            fields = strip_line.split(',')
+            field = fields[1]
+            outblock_field.append(field)
+
+    data['inblock'] = data['name'] + "InBlock"
+    data['outblock'] = data['name'] + "OutBlock"
+    data['inblock_field'] = inblock_field
+    data['outblock_field'] = outblock_field
+    return data
+
+
+if __name__ == "__main__":
+    f = open("c:/eBEST/xingAPI/Res/t8430.res", encoding="euc-kr")
+    lines = f.readlines()
+    f.close()
+
+    import pprint
+    data = parse_res(lines)
+    pprint.pprint(data)
